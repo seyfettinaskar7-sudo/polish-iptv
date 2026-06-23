24e34eb8143dc4ccfaef8be599b0a00d0d4048c5
seyfettinaskar7-sudo <seyfettinaskar7@gmail.com>
2026-06-23 21:46:36 +0300

    Update TVPVOD.ts

--- worker/SRC/TVPVOD.ts
+++ worker/SRC/TVPVOD.ts
@@ -52,6 +52,7 @@ const CHANNELS: readonly Channel[] = [
   { id: "399749", slug: "tvpolsztyn",  name: "TVP3 Olsztyn", logo: TVP_LOGO, group: "Polska" },
   { id: "399750", slug: "tvpopole",    name: "TVP3 Opole",   logo: TVP_LOGO, group: "Polska" },
   { id: "399751", slug: "tvppoznan",   name: "TVP3 Poznan",  logo: TVP_LOGO, group: "Polska" },
+  { id: "399752", slug: "tvprzesrow",  name: "TVP3 Rzesrow", logo: TVP_LOGO, group: "Polska" },
   { id: "399699", slug: "tvpinfo",     name: "TVP Info",     logo: TVP_LOGO, group: "Polska" },
   { id: "2543050",slug: "tvpkryminaly",name: "TVP Kryminaly",logo: TVP_LOGO, group: "Polska" },
   { id: "1065595",slug: "tvpparlament",name: "TVP Parlament",logo: TVP_LOGO, group: "Polska" },
